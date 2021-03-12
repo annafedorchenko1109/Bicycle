329c2d8969186004cb20b60e68ec1c409342751b
annafedorchenko1109 <72519487+annafedorchenko1109@users.noreply.github.com>
2021-03-12 12:16:52 +0200

    Add files via upload

---
+++ sourse/js/script.js
@@ -0,0 +1,7 @@
+$('.wrapper').addClass('loaded');
+
+$('.icon-menu').click(function(event) {
+	$(this).toggleClass('active');
+	$('.menu__body').toggleClass('active');
+	$('body').toggleClass('lock');
+});
